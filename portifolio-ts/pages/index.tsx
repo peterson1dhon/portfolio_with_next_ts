ed7b611e8433cafb250241ba30b22fb49e966ad5
peterson1dhon <peterson_dhon@hotmail.com>
2022-11-11 12:16:26 -0300

    arow function

--- portifolio-ts/pages/index.tsx
+++ portifolio-ts/pages/index.tsx
@@ -1,9 +1,9 @@
+import { NextPage } from 'next';
 import Head from 'next/head'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
-import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
@@ -26,5 +26,7 @@ export default function Home() {
 
       {/* Contact me */}
     </div>
-  )
-}
+  );
+};
+
+export default Home;
