9184f24f274cf4909400bed8b5044edfd6e1651f
peterson1dhon <peterson_dhon@hotmail.com>
2022-12-04 12:55:24 -0300

    criação da seção contate-me

--- portifolio-ts/components/ContactMe.tsx
+++ portifolio-ts/components/ContactMe.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { PhoneIcon,MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid';
 
 type Props = {}
 
@@ -8,8 +9,45 @@ function ContactMe({}: Props) {
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
             Contate-me
         </h3>
+
+        <div className='flex flex-col space-y-10'>
+          <h4 className='text-4xl font-semibold text-center'>
+            Eu tenho apena o que você precisa.{" "}
+            <span className='decoration-[#F7AB0A]/50 underline'>vamos conversar</span>
+          </h4>
+
+          <div className='space-y-10'>
+            <div className='flex items-center space-x-5 justify-center'>
+              <PhoneIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
+              <p className='text-2xl'>+55 (15) 99785-1492</p>
+            </div>
+
+            <div className='flex items-center space-x-5 justify-center'>
+              <EnvelopeIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
+              <p className='text-2xl'>peterson.br94@gmail.com</p>
+            </div>
+
+            <div className='flex items-center space-x-5 justify-center'>
+              <MapPinIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
+              <p className='text-2xl'>Jacó Abrame 561 - Tatuí/SP</p>
+            </div>
+          </div>
+
+          <form className='flex flex-col space-y-2 w-fit mx-auto'>
+            <div className='flex space-x-2'>
+              <input className='contactInput' type="text" />
+              <input className='contactInput' type="text" />
+            </div>
+
+            <input className='contactInput' type="text" />
+
+            <textarea className='contactInput' />
+            <button>Enviar</button>
+          </form>
+          
+        </div>
     </div>
-  )
+  );
 }
 
-export default ContactMe
+export default ContactMe;
